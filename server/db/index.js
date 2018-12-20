4cf2942ba49067ef88866b07c48612e55e65ca14
Rudy <rudydimacali@gmail.com>
2018-12-20 11:11:59 -0800

    Initialize server/db/index.js.

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,7 @@ const mysqlConfig = require("./config.js");
 const Sequelize = require("sequelize");
 const faker = require("faker");
 
+console.log(mysqlConfig);
 // 'mysql://{user}:{password}@{IP}/{databaseName}'
 const sequelize = new Sequelize(mysqlConfig);
 
@@ -14,6 +15,38 @@ sequelize
     throw err;
   });
 
-const Restaurant = sequelize.define("restaurant", {
-  restaurantName: Sequelize.STRING
+const Restaurants = sequelize.define(
+  "restaurants",
+  {
+    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+    restaurantName: Sequelize.STRING
+  },
+  {
+    timestamps: false
+  }
+);
+
+const Reservations = sequelize.define(
+  "reservations",
+  {
+    restaurantID: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Restaurants,
+        key: "id"
+      }
+    },
+    dateToReserve: Sequelize.INTEGER,
+    timeToReserve: Sequelize.INTEGER,
+    partySize: Sequelize.INTEGER
+  },
+  {
+    timestamps: false
+  }
+);
+
+Restaurants.sync().then(() => {
+  return Restaurants.create({
+    restaurantName: "LihoLiho Yacht Club"
+  });
 });
