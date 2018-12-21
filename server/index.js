d7b236a8e8ccdeab5c4213042038c30160d5277f
Rudy <rudydimacali@gmail.com>
2018-12-21 11:14:17 -0800

    Change url to route index serving and get/post requests.

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const db = require('../db');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../client/dist`));
+app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser());
 
-app.get('/reservations/restaurantID=:restaurantID&date=:date&time=:time&partySize=:partySize', (req, res) => {
+app.get('/api/reservations/restaurantID=:restaurantID&date=:date', (req, res) => {
   db.getReservations(req.params.restaurantID, req.params.date, (results) => {
     console.log('results', JSON.parse(results));
     res.end(results);
   });
 });
 
-app.post('/reservations/restaurantID=:restaurantID&date=:date&time=:time&partySize=:partySize', (req, res) => {
+app.post('/api/reservations/restaurantID=:restaurantID&date=:date&time=:time&partySize=:partySize', (req, res) => {
   db.addReservation(req.params.restaurantID, req.params.date, req.params.time, req.params.partySize, () => {
     res.end();
   });
