a258423e2d69696385a8d1fd38999825a294e26a
Rudy <rudydimacali@gmail.com>
2018-12-21 11:23:25 -0800

    Adjust seed to exit after completion.

--- db/seed.js
+++ db/seed.js
@@ -20,5 +20,7 @@ for (let i = 0; i < 99; i += 1) {
 }
 
 for (let i = 0; i < 98; i += 1) {
-  db.addReservation(generateRestaurantId(), generateDate(), generateTime(), generatePartySize());
+  db.addReservation(generateRestaurantId(), generateDate(), generateTime(), generatePartySize(), () => {
+
+  });
 }
