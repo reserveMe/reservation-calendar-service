3117bb746ac9b01b841b703fcc82c4dcbbc1aedb
Rudy <rudydimacali@gmail.com>
2018-12-30 18:17:27 -0800

    Move global style into separate file.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,48 +3,7 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Widget from './components/Widget/Widget.jsx';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Aleo');
- 
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: 'Aleo', sans-serif;
-    background-color: #f1f2f4;
-  }
-
-  input {
-    cursor: pointer;
-    padding: 0;
-    height: 100%;
-    width: 100%;
-    &:hover{
-      border-bottom: 1px solid #da3743;
-    }
-    border-radius: 5px;
-  }
-
-  .react-datepicker-wrapper {
-    margin: 0 auto;
-    cursor: pointer;
-    border-right: none;
-    position: relative;
-    height: 33px;
-    width: 95%;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    border-bottom: 1px solid #d8d9db;
-  }
-
-  .react-datepicker__input-container {
-    margin: 0 auto;
-    height: 100%;
-    width: 100%;
-  }
-
-`;
+import GlobalStyle from './globalstyle';
 
 const format = require('date-fns/format');
 
