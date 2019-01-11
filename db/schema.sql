7b865582460d1b1afa984466ebb854f97bbed4e8
Kevin Chin <chinkevin25@gmail.com>
2019-01-10 21:06:04 -0800

    fix schema for psql

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,30 @@
-DROP DATABASE IF EXISTS openTableReservations;
+-- DROP DATABASE IF EXISTS openTable;
 
-CREATE DATABASE openTableReservations;
+-- CREATE DATABASE openTableReservations;
 
-USE openTableReservations;
+-- USE openTableReservations;
 
 /* Create other tables and define schemas for them here! */
 
 CREATE TABLE restaurants (
-  id int NOT NULL AUTO_INCREMENT,
-  restaurantName varchar(40),
+  id SERIAL,
+  name varchar(40) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE reservations (
-  id int NOT NULL AUTO_INCREMENT,
+  id SERIAL,
   restaurantID int NOT NULL,
   dateToReserve varchar(6) NOT NULL,
   timeToReserve varchar(4) NOT NULL,
   partySize varchar(2) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (restaurantID) REFERENCES restaurants(id)
+  FOREIGN KEY (restaurantID) REFERENCES restaurants(id) ON DELETE CASCADE
 );
 
-INSERT INTO restaurants (restaurantName) VALUES ('The Saratoga');
-INSERT INTO reservations (restaurantID, dateToReserve, timeToReserve, partySize) VALUES (1, '021319', '1530', '4');
-INSERT INTO reservations (restaurantID, dateToReserve, timeToReserve, partySize) VALUES (1, '021319', '1230', '2');
+-- INSERT INTO restaurants (restaurantName) VALUES ('The Saratoga');
+-- INSERT INTO reservations (restaurantID, dateToReserve, timeToReserve, partySize) VALUES (1, '021319', '1530', '4');
+-- INSERT INTO reservations (restaurantID, dateToReserve, timeToReserve, partySize) VALUES (1, '021319', '1230', '2');
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root -p < server/schema.sql
