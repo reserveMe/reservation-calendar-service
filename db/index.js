d0e799e93ba93bdb1ba3d159be273215ad68d713
Kevin Chin <chinkevin25@gmail.com>
2019-01-16 19:40:06 -0800

    implement all CRUD routes

--- db/index.js
+++ db/index.js
@@ -1,95 +1,35 @@
 const { Pool } = require('pg');
 const config = require('./config.js');
 
-// 'mysql://{user}:{password}@{IP}/{databaseName}'
-// const sequelize = new Sequelize(mysqlConfig);
-// const sequelize = new Sequelize(`mysql://root:mysql1passres@172.17.0.2/openTableReservations`)
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Successfully connected to database.');
-//   })
-//   .catch((err) => {
-//     throw err;
-//   });
-
 const pool = new Pool(config);
 
-// const Restaurants = sequelize.define(
-//   'restaurants',
-//   {
-//     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-//     restaurantName: Sequelize.STRING,
-//   },
-//   {
-//     timestamps: false,
-//   });
-
-// const Reservations = sequelize.define(
-//   'reservations',
-//   {
-//     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-//     restaurantID: {
-//       type: Sequelize.STRING,
-//       references: {
-//         model: Restaurants,
-//         key: 'id',
-//       },
-//     },
-//     dateToReserve: Sequelize.STRING,
-//     timeToReserve: Sequelize.STRING,
-//     partySize: Sequelize.STRING,
-//   },
-//   {
-//     timestamps: false,
-//   });
-
-const addRestaurant = (restaurantName) => {
-  // Restaurants.sync()
-  //   .then(() => Restaurants.create({ restaurantName }));
-  return pool.query(`INSERT INTO restaurants (name) VALUES (${restaurantName})`);
-};
-
 const addReservation = (restaurantID, dateToReserve, timeToReserve, partySize, callback) => {
-  // return Reservations.sync()
-  //   .then(() => Reservations.create({
-  //     restaurantID,
-  //     dateToReserve,
-  //     timeToReserve,
-  //     partySize,
-  //   }))
-  //   .then(() => {
-  //     callback();
-  //   })
-  //   .catch((err) => { throw err; });
   pool.query(`INSERT INTO reservations (restaurantid, date, time, party, string) VALUES (${restaurantID}, ${dateToReserve}, ${timeToReserve}, ${partySize}, 'INSERT');`)
-    .then(result => callback(false, result))
-    .catch(err => callback(true, err));
+    .then(result => callback(null, result))
+    .catch(err => callback(err));
 };
 
 const getReservations = (restaurantID, dateToReserve, callback) => {
+  pool.query(`SELECT * FROM reservations WHERE restaurantid = ${restaurantID} AND date = ${dateToReserve}`)
+    .then(results => callback(null, results.rows))
+    .catch(err => callback(err));
+};
 
-  pool.query(`SELECT * FROM reservations WHERE restaurantid = ${restaurantID}`)
-    .then((results) => {
-      callback(false, results.rows);
-    })
-    .catch(err => callback(true, err));
-  // Reservations.sync()
-  //   .then(() => {
-  //     return Reservations.findAll({
-  //       where: {
-  //         restaurantID,
-  //         dateToReserve,
-  //       },
-  //     });
-  //   })
-  //   .then(results => callback(JSON.stringify(results)))
-  //   .catch((err) => { throw err; });
+const deleteReservation = (restaurantID, dateToDelete, timeToDelete, callback) => {
+  pool.query(`DELETE FROM reservations WHERE restaurantid = ${restaurantID} AND date = '${dateToDelete}' AND time = '${timeToDelete}'`)
+    .then(() => callback(null))
+    .catch(err => callback(err));
+};
+
+const updateReservation = (restaurantID, oldDate, oldTime, newDate, newTime, callback) => {
+  pool.query(`UPDATE reservations SET date = '${newDate}', time = ${newTime} WHERE restaurantid = ${restaurantID} AND date = '${oldDate}' AND time = '${oldTime}'`)
+    .then(response => callback(null, response))
+    .catch(err => callback(err));
 };
 
 module.exports = {
-  addRestaurant,
   addReservation,
   getReservations,
+  deleteReservation,
+  updateReservation,
 };
--- server/index.js
+++ server/index.js
@@ -26,9 +26,32 @@ app.post('/api/reservations/', (req, res) => {
   console.log(req.body);
   db.addReservation(req.body.restaurantID, req.body.date, req.body.time, req.body.partySize, (err) => {
     if (err) {
+      console.log(err);
       res.sendStatus(400);
     } else {
       res.end('Reservation Created');
+    }
+  });
+});
+
+app.delete('/api/reservations', (req, res) => {
+  db.deleteReservation(req.body.restaurantID, req.body.date, req.body.time, (err) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      res.end('Reservation Deleted');
+    }
+  });
+});
+
+app.patch('/api/reservations', (req, res) => {
+  db.updateReservation(req.body.restaurantID, req.body.oldDate, req.body.oldTime, req.body.newDate, req.body.newTime, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      res.end('UPDATED');
     }
   });
 });
